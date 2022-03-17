b2f5eaa78a175aacff7d50abb40427301899b654
Cristian Livella <cristian@cristianlivella.com>
2022-03-17 23:04:08 +0100

    🧐 Save app version in local storage (plan to use it when migrationg to v2, to show a welcome/changelog modal)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { useEffect } from 'react';
+
 import './App.css';
 import HomePage from './components/pages/HomePage/HomePage';
 
 function App() {
+    useEffect(() => {
+        localStorage.setItem('cryptax_version', '1');
+    }, []);
+
     return (
         <div className='App'>
             <HomePage />
